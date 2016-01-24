3ac7a9677bd44a068b8aafaa6f8a3bc74169056b
FrankSong <FrankSong@e23a610f-7b4d-4d97-9b41-d932ca0f6786>
2016-01-24 08:55:40 +0000

    some more fields

--- UofTHacks-Java/UofTHacks/app/src/main/java/uofthacks/myapplication/EnterClothingFieldActivity.java
+++ UofTHacks-Java/UofTHacks/app/src/main/java/uofthacks/myapplication/EnterClothingFieldActivity.java
@@ -3,6 +3,10 @@ package uofthacks.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.HashMap;
 
@@ -20,6 +24,22 @@ public class EnterClothingFieldActivity extends AppCompatActivity {
         HashMap<String, String> curr_map =
                 (HashMap<String, String>) intent.getSerializableExtra(
                         "HashMap-Fields");
+
+
+        // Freeze the fields depending on the class
+        if (curr_map.containsKey("Accessories")) {
+            // freeze size and colour
+            TextView sizeField = (TextView) findViewById(R.id.sizeField);
+            TextView colourField = (TextView) findViewById(R.id.colourField);
+            sizeField.setFocusable(false);
+            colourField.setFocusable(false);
+        }
+
+
+    }
+
+    public void proceedToSpecificFields(View view) {
+//        Intent intent = new Intent(this,);
     }
 
 }
